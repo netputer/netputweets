65d23bf878aae5ca5e3aabac85ae96191f780c3f
Comic Chang <comicchang@gmail.com>
2013-03-08 11:23:02 +0800

    typo fix

--- config-sample.php
+++ config-sample.php
@@ -7,7 +7,7 @@ define('EMBEDLY_KEY', 'putyourinfohere');
 
 define('BASE_URL', 'putyourinfohere');
 $relative_url = ($directory = trim(dirname($_SERVER["SCRIPT_NAME"]), "/\,")) ? "/$directory/" : "/";
-define('RELATIVE_URL', $relativee_url);
+define('RELATIVE_URL', $relative_url);
 
 define('NPT_TITLE', 'putyourinfohere');
 define('API_ROOT', 'https://api.twitter.com/1.1/');
