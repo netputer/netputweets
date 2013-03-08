ae17b8f5b5e796616e9da236838d71ffdb640041
Comic Chang <comicchang@gmail.com>
2013-03-08 12:37:32 +0800

    add adaptive BASE_URL in config

--- config-sample.php
+++ config-sample.php
@@ -1,11 +1,25 @@
 <?php
+
+
 define('ENCRYPTION_KEY', 'putyourinfohere');
 
 define('OAUTH_KEY', 'putyourinfohere');
 define('OAUTH_SECRET', 'putyourinfohere');
 define('EMBEDLY_KEY', 'putyourinfohere');
 
-define('BASE_URL', 'putyourinfohere');
+//define BASE_URL as you want
+define('BASE_URL_CONFIG', 'putyourinfohere');
+
+$base_url = BASE_URL_CONFIG;
+//else we will figure it out
+if (!$base_url) {
+    $base_url = isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] != "off" ? "https" : "http";
+    $base_url .= "://".$_SERVER["HTTP_HOST"];
+    $base_url .= ($directory = trim(dirname($_SERVER["SCRIPT_NAME"]), "/\,")) ? "/$directory/" : "/";
+}
+
+define('BASE_URL', $base_url);
+
 $relative_url = ($directory = trim(dirname($_SERVER["SCRIPT_NAME"]), "/\,")) ? "/$directory/" : "/";
 define('RELATIVE_URL', $relative_url);
 
